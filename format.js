5f4eb00a6bc7280290776f9a3362a355876366d5
Phoenix <phnixkane@gmail.com>
2021-06-15 13:49:36 -0700

    Add files via upload
    -finalized javascript, edited css, and began formatting html for final game

--- format.js
+++ format.js
@@ -1,17 +1,182 @@
+let currentRound = 1;
 
+let bountyTotal = 0;
 
-//if (crimesOps.length > 0) {
-//  var crimeList = document.getElementsByTagName('a');
-//  console.log(crimeList);
-//
-//  Array.from(crimeList).forEach((crime) => {
-//     console.log(crime);
-//  });
+let captureChance = 0;
+let dangerLevel = "Low";
+let hasBribed = false;
 
-//  console.log(crimeList);
-//  console.log(crimes);
-//}
+let playerLoc = [3, 7];
+let playerLastLoc = [3, 7];
+let ramonLoc = [10, 27];
+let viperLoc = [55, 2];
 
+let playerSpeed = 4;
+let ramonSpeed = 4;
+let viperSpeed = 6;
+
+let ramonEncounter = false;
+let viperEncounter = false;
+
+// If the player clicks anywhere on the page, click the link marked "next"
 $(document).on("mousedown", function (event) {
 	$(".next").find('a').trigger("click");
+
+	// If there isn't a link marked "next", find the appropriate link
+	if (ramonEncounter == true) {
+		$(".ramon").find('a').trigger("click");
+	} else if (viperEncounter == true) {
+		$(".viper").find('a').trigger("click");
+	} else {
+		$(".safe").find('a').trigger("click");
+	}
 });
+
+// Determine the player's level of danger
+function dangerManager() {
+	// Find the distance between the player and Ramon
+	let shortestDistance = checkDistance(playerLoc, ramonLoc);
+
+	// If it's after Round 1, find the distance between the player and Viper
+	// Find out who the player is closer to
+	if (currentRound > 1) {
+		let currentDanger = checkDistance(playerLoc, viperLoc);
+		if (currentDanger < shortestDistance) {
+			shortestDistance = currentDanger;
+		}
+	}
+	return checkDanger(shortestDistance);
+}
+
+// Check the distance between the player and something else
+function checkDistance(player, alt) {
+	let lengthX = alt[0] - player[0];
+	let lengthY = alt[1] - player[1];
+
+	// Use Pythagorean Theorum to find the distance
+	let distance = Math.sqrt(Math.abs(lengthX * lengthX) + Math.abs(lengthY * lengthY));
+
+	// Convert distance into an integer, rounding down
+	return Math.floor(distance);
+}
+
+// Return the semantic value of danger based on player's distance from Hunter
+function checkDanger(distance) {
+	if (distance < 4) { 				 // Hunter is within a minute away
+		return "Extreme";
+	} else if (distance <= 8) {	 // Hunter is 1-2 minutes away
+		return "High";
+	} else if (distance <= 16) { // Hunter is 2-4 minutes away
+		return "Moderate";
+	} else if (distance > 16) {  // Hunter is more than 4 minutes away
+		return "Low";
+	} else {
+		return "Error";
+	}
+}
+
+// Basically the move manager - used to check for crime danger AND for actually moving around
+function timeAdjust(crimeLoc, crimeTime, doMove) {
+	var dist = checkDistance(playerLoc, crimeLoc);	// Get the distance from character to goal
+	var time = Math.floor(crimeTime + (dist / 4));  // Get travel/crime time, rounded to a whole integer
+	captureChance = 0 															// Reset the chance of being captured
+
+	// If we're just checking, move dummies, not the real characters
+	if (doMove == false) {
+		var tempChar = playerLoc;
+		var tempRamon = ramonLoc;
+		var tempViper = viperLoc;
+
+		for (var i = 0; i < time; time -= 1) {
+			// Move the dummy player towards the crime
+			moveChar(tempChar, crimeLoc, playerSpeed);
+
+			// Move the dummy Hunter(s) towards the player's last known position
+			moveChar(tempRamon, playerLastLoc, ramonSpeed);
+			if (currentRound > 1) {
+				moveChar (tempViper, playerLastLoc, viperSpeed);
+			};
+
+			if (dangerLevel == "Extreme") {
+				testDanger(captureChance);
+			}
+
+			dangerManager()
+			console.log(dangerManager());
+		}
+
+	} else { 												// Otherwise, really move the characters
+		playerLastLoc = playerLoc;		// Set the player's last known position
+
+		for (var i = 0; i < time; time -= 1) {
+			// Move the player towards the crime
+			moveChar(playerLoc, crimeLoc, playerSpeed);
+
+			// Move the Hunter(s) towards the player's last known position
+			moveChar(ramonLoc, playerLastLoc, ramonSpeed);
+			if (currentRound > 1) {
+				moveChar (viperLoc, playerLastLoc, viperSpeed);
+			};
+
+			// Update the player UI based on dangerLevel
+			dangerLevel = dangerManager();
+
+			// For every minute the player is in extreme danger, raise the chance of being captured
+			if (dangerLevel == "Extreme") {
+				dangerDanger(captureChance);
+			}
+		}
+	}
+
+	console.log("Final danger level is " + dangerManager());
+}
+
+// Figure out the angle of the distance from character to their goal
+//   and then move them along that angle by their speed
+function moveChar(char, goal, spd) {
+	var dist = checkDistance(char, goal)
+	var angle = Math.atan2(goal[1] - char[1], goal[0] - char[0]);
+
+	// Set the character's coords to their goal's coords if they're close enough
+	if (dist > 4) {
+		char[0] += Math.cos(angle) * spd;
+		char[1] += Math.sin(angle) * spd;
+		dist -= spd;
+	} else {
+		char[0] = goal[0];
+		char[1] = goal[1];
+	}
+}
+
+function testDanger(chance) {
+	let rand = Math.random() * 100;
+
+	if (chance > rand) {
+
+	} else {
+		captureChance += 40;
+	}
+}
+
+function dangerDanger(chance) {
+	let rand = Math.random() * 100;
+
+	if (chance > rand) {
+		// Find out who's putting the player in danger
+		var ramonDist = checkDistance(playerLoc, ramonLoc);
+		var viperDist = checkDistance(playerLoc, viperLoc);
+		var closest = Math.min(ramonDist, viperDist);
+
+		// Find the appropriate Hunter's encounter page and send the player there
+		switch (closest) {
+			case ramonDist:
+				ramonEncounter = true;
+				break;
+			case viperDist:
+				viperEncounter = true;
+				break;
+		}
+	} else {
+		captureChance += 40;
+	}
+}
